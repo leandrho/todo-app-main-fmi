1ad90f5ab34b50f8f824e7cc82a036bd480a469f
Leandrho <leandro.coder01@gmail.com>
2024-11-12 09:34:14 -0300

    styles TodoItem

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,7 @@
 import { useId, useRef } from 'react';
 import { TodoItemType } from '../types/index';
+import c from './TodoItem.module.css';
+
 
 type TodoItemProps = {
     item: TodoItemType,
@@ -19,30 +21,40 @@ export const TodoItem = ({ item, removeItem, modifyItem }:TodoItemProps) => {
       const mitem :TodoItemType = {...item, done: d};
       modifyItem(mitem);
     }
-    
   }
 
   return (
-    <div className=''>
-      <div className="">
-        <div className=''>
-            <input ref={checkinput} id={id} type='checkbox' aria-label="submit" 
-                  className=""
-                  checked={item.done}
-                  onChange={()=>changeDone()}
-            />
-            <label htmlFor={id} aria-label="submit label" className=''>
-              <img src="./images/icon-check.svg" alt="" />
-            </label>
-            <input ref={input} aria-label="Enter new task" type="text" className="" value={item.description} readOnly/>
+    <div className={c.container}>
+      <div className={c.item}>
+        <div className={c.item__content}>
+          <input ref={checkinput} 
+                id={id} 
+                type='checkbox' 
+                aria-label="submit" 
+                className={c.item__checkbox}
+                checked={item.done}
+                onChange={()=>changeDone()}
+          />
+          <label htmlFor={id} 
+                  aria-label="submit label" 
+                  className={c.item__label}
+          >
+          </label>
+          <input ref={input}
+                  id={id + '_titem_text'} 
+                  aria-label="Todo text" 
+                  type="text" 
+                  className={c.item__text}
+                  value={item.description} 
+                  readOnly
+          />
         </div>
-
-        <button className=''
+        <button className={c.item__btn}
+                aria-label='remove item'
                 onClick={()=>removeItem(item.id)}
         >
-            <img src="./images/icon-cross.svg" alt="" />
-        </button>
-        
+          <svg className={c.item__btn_svg} xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="currentColor" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+        </button>   
       </div>
     </div>
   )
